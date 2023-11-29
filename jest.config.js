622c4acfcf6bba299cb7165c950a6f07df8a5ed1
yuqiaosu-toast <Simon.Suyq@gmail.com>
2023-11-29 18:59:04 -0500

    Trying to add jest test

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/prisma/test/*.test.js'],
+  setupFilesAfterEnv: ['<rootDir>/prisma/test/singleton.ts'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx']
+};
