965683a411703ef5cdbb6280c4326a68b4316574
yuqiaosu-toast <Simon.Suyq@gmail.com>
2023-11-29 19:40:46 -0500

    Unit test for Prisma

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/prisma/test/*.test.js'],
+  testMatch: ['<rootDir>/prisma/test/*.test.ts'],
   setupFilesAfterEnv: ['<rootDir>/prisma/test/singleton.ts'],
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx']
 };
