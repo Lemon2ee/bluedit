aa40d423906894484f983151458a76e7be0b86e3
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2023-12-01 13:11:41 -0500

    not yet done. need implement the profile fetch api for publics

---
+++ app/profile/[userID]/page.tsx
@@ -0,0 +1,49 @@
+import Navbar from "@/app/home/navbar";
+import { headers } from "next/headers";
+import { ProfileUpdateData } from "@/lib/type";
+import Header from "@/app/profile/header";
+
+async function getProfile(id: string) {
+  const host = headers().get("host");
+  const protocol = process?.env.NODE_ENV === "development" ? "http" : "https";
+  const res = await fetch(`${protocol}://${host}/api/profile/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function PublicProfile({
+  params,
+}: {
+  params: { userID: string };
+}) {
+  const userID = params.userID;
+  if (!userID) return null;
+
+  const profile: ProfileUpdateData = await getProfile(userID);
+
+  if (
+    !profile ||
+    !profile.profilePicture ||
+    !profile.bannerPicture ||
+    !profile.displayName
+  )
+    return null;
+
+  return (
+    <>
+      <Navbar />
+      <Header
+        profilePicture={profile.profilePicture}
+        bannerPicture={profile.bannerPicture}
+        name={profile.displayName}
+      />
+    </>
+  );
+}
