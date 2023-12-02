f2cf3e7a942fd84faf82862b31602f4de8e6bf3e
Yuqiao-Su <Simon.Suyq@gmail.com>
2023-12-02 15:32:53 -0500

    Showing the content of the post, UI is not well structured.

--- app/thread/page.tsx
+++ app/thread/page.tsx
@@ -6,6 +6,10 @@ import {useRouter} from "next/navigation";
 
 export default function Home() {
     const router = useRouter();
+    const navigateToThread = () => {
+        const threadId = '656a9da7a331a9d71db3c72e';
+        router.push(`/thread/${threadId}`);
+    };
     return (
         <>
             <Navbar/>
@@ -14,6 +18,12 @@ export default function Home() {
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
                 Create post
+            </button>
+            <button
+                onClick={navigateToThread}
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+                View post
             </button>
 
         </>
