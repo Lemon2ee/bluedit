a1550a81806d8c8f5c12b39776be04dd5f8954fb
Sebastian Wang <wang.yizho@northeastern.edu>
2023-12-04 14:11:54 -0500

    comment id change

--- app/profile/[[...profileID]]/page.tsx
+++ app/profile/[[...profileID]]/page.tsx
@@ -1,69 +1,68 @@
 import Navbar from "@/app/home/NavBar/navbar";
 import Header from "@/app/profile/header";
-import {getServerSession} from "next-auth";
+import { getServerSession } from "next-auth";
 import authOptions from "@/app/api/auth/[...nextauth]/nextAuthOptions";
-import {$Enums} from ".prisma/client";
+import { $Enums } from ".prisma/client";
 import Role = $Enums.Role;
 import prisma from "@/lib/prisma";
 import ProfileCommentsList from "@/app/profile/[[...profileID]]/profileCommentsList";
 
 export interface CommentWithThreadInfo {
-    id: string;
-    content: string;
+  id: string;
+  content: string;
+  createdAt: Date;
+  authorId: string;
+  threadId: string;
+  thread: {
     createdAt: Date;
-    authorId: string;
-    threadId: string;
-    thread: {
-        createdAt: Date;
-        title: string;
-    };
+    title: string;
+  };
 }
 
 async function getUserComments(userID: string) {
-    return prisma.comment.findMany({
-        where: {
-            authorId: userID,
+  return prisma.comment.findMany({
+    where: {
+      authorId: userID,
+    },
+    include: {
+      thread: {
+        select: {
+          createdAt: true,
+          title: true,
         },
-        include: {
-            thread: {
-                select: {
-                    createdAt: true,
-                    title: true,
-                },
-            },
-        },
-    });
+      },
+    },
+  });
 }
 
 export default async function PublicProfile({
-                                                params,
-                                            }: {
-    params: {
-        profileID: string[];
-    };
+  params,
+}: {
+  params: {
+    profileID: string[];
+  };
 }) {
-    let profileUserID = "";
-    let editable = false;
-    const session = await getServerSession(authOptions);
-    if (!session?.user.id || !session.user.role) return null;
+  let profileUserID = "";
+  let editable = false;
+  const session = await getServerSession(authOptions);
+  if (!session?.user.id || !session.user.role) return null;
 
-    if (params.profileID == undefined) {
-        profileUserID = session?.user?.id;
-        editable = true;
-    } else if (params.profileID.length == 1) {
-        profileUserID = params.profileID[0];
-        editable = session.user.role === Role.ADMIN;
-    }
+  if (params.profileID == undefined) {
+    profileUserID = session?.user?.id;
+    editable = true;
+  } else if (params.profileID.length == 1) {
+    profileUserID = params.profileID[0];
+    editable = session.user.role === Role.ADMIN;
+  }
 
-    const comments: CommentWithThreadInfo[] = await getUserComments(
-        session.user.id,
-    );
+  const comments: CommentWithThreadInfo[] =
+    await getUserComments(profileUserID);
 
-    return (
-        <>
-            <Navbar/>
-            <Header editable={editable} profileUserID={profileUserID}/>
-            <ProfileCommentsList comments={comments}/>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Header editable={editable} profileUserID={profileUserID} />
+      <ProfileCommentsList comments={comments} />
+    </>
+  );
 }
