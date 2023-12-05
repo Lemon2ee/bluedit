4e9422615558fd7cca382afd7e4831b0c87e9ad3
Yuqiao-Su <Simon.Suyq@gmail.com>
2023-12-04 21:54:11 -0500

    1

--- middleware.js
+++ middleware.js
@@ -1,11 +1,11 @@
-export { default } from "next-auth/middleware";
+export {default} from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    "/profile",
-    "/profile/edit",
-    "/api/thread/:threadID/createComment",
-    "/api/thread/:threadID/upvote",
-    "/api/thread/",
-  ],
+    matcher: [
+
+        "/profile/edit",
+        "/api/thread/:threadID/createComment",
+        "/api/thread/:threadID/upvote",
+        "/api/thread/",
+    ],
 };
