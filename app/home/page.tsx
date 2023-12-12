cd2e64668a60e992b977b18c5854af54d9282821
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2023-12-11 23:20:14 -0500

    remove redundant login check

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -49,10 +49,8 @@ async function getAllThreads(
 export default async function Home() {
   const cookie = cookies();
 
-  console.log(cookie);
-
   const threads: ThreadWithProfile[] = await getAllThreads(
-    cookie.get("login")?.value === "true",
+    cookie.get("next-auth.session-token") !== undefined,
   );
 
   threads.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
